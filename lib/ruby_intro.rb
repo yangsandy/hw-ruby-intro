e4bad98c5c50bb4e99a9bad2d82264d0c8c43c1f
EC2 Default User <ec2-user@ip-172-31-95-220.ec2.internal>
2018-02-12 18:40:55 +0000

    sum max_of_2 done

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,10 +4,47 @@
 
 def sum arr
   # YOUR CODE HERE
+  if arr.length==0
+    return 0
+  end
+  i=0
+  res=0
+  while i<arr.length
+    res+=arr[i]
+    i+=1
+  end
+  return res
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  if arr.length==0
+    return 0
+  end
+  if arr.length==1
+    return arr[0]
+  end
+  
+  #Source: https://stackoverflow.com/questions/535721/ruby-max-integer
+  fixnum_min = -(2**(0.size * 8 -2))
+  
+  i=0
+  largest=fixnum_min
+  snd_largest=fixnum_min
+  
+  while i<arr.length
+    if arr[i]>snd_largest
+      if arr[i]>largest
+        snd_largest=largest
+        largest=arr[i]
+      else
+        snd_largest=arr[i]
+      end
+    end
+    i+=1
+  end
+  
+  return largest+snd_largest
 end
 
 def sum_to_n? arr, n
